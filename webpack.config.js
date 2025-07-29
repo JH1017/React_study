b291c3a18ae1e2ff722d0d41b3f51c04e0708321
JunhwanKang <wnsghks1017@naver.com>
2025-07-29 22:06:33 +0900

    웹팩 사용해보기 250729

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,19 @@
+const path = require('path');
+
 module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js','jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    }, // 입력
+    output: {
+        path: path.jojn(__dirname, 'dist'), // 현재 폴더를 가져온다.
+        filename: 'app.js'
+    } // 출력
 
 };
